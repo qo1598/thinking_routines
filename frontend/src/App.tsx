8a10d3d37063444cbf856ee2b8baad43dd21a0e2
qo1598 <qo1598@dge.go.kr>
2025-07-14 23:06:28 +0900

    채널톡(상담 버튼) 연동 적용
    # Conflicts:
    #	frontend/src/components/LandingPage.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import TeacherLogin from './components/TeacherLogin';
@@ -8,22 +8,28 @@ import StudentResponseDetail from './components/StudentResponseDetail';
 import StudentEntry from './components/StudentEntry';
 import SeeThinkWonderForm from './components/SeeThinkWonderForm';
 import './App.css';
+import * as ChannelService from '@channel.io/channel-web-sdk-loader';
 
 function App() {
+  useEffect(() => {
+    ChannelService.loadScript();
+    ChannelService.boot({
+      pluginKey: '31d0c99e-1966-4296-b7bd-208b69dba1e0',
+    });
+  }, []);
+
   return (
     <Router>
     <div className="App">
         <Routes>
           {/* 기본 경로를 랜딩 페이지로 설정 */}
           <Route path="/" element={<LandingPage />} />
-          
           {/* 교사 관련 경로 */}
           <Route path="/teacher" element={<TeacherLogin />} />
           <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
           <Route path="/teacher/thinking-routines" element={<TeacherDashboard />} />
           <Route path="/teacher/room/:roomId" element={<TeacherRoomDetail />} />
           <Route path="/teacher/room/:roomId/response/:responseId" element={<StudentResponseDetail />} />
-          
           {/* 학생 관련 경로 */}
           <Route path="/student" element={<StudentEntry />} />
           <Route path="/student/activity/:roomId" element={<SeeThinkWonderForm />} />
